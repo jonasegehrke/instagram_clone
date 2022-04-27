283a28f4adc96298344494dae44053ffeafa246d
Jonas Gehrke <jonasegherke@gmail.com>
2022-04-27 16:11:27 +0200

    signup & Snackbar progress

--- lib/authentication_service.dart
+++ lib/authentication_service.dart
@@ -18,6 +18,7 @@ class AuthenticationService{
       return "Signed in";
     } on FirebaseAuthException catch (e){
       return e.message;
+      
     }
   }
 
--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -5,40 +5,76 @@ import 'package:instagram_clone/authentication_service.dart';
 import 'package:provider/provider.dart';
 
 class SignInPage extends StatelessWidget {
-
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            controller: emailController,
-            decoration: const InputDecoration(
-              labelText: "Email",
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: "Email",
+              ),
             ),
-          ),
-          TextField(
-            controller: passwordController,
-            decoration: const InputDecoration(
-              labelText: "Password",
+            TextField(
+              obscureText: true,
+              controller: passwordController,
+              decoration: const InputDecoration(
+                labelText: "Password",
+              ),
             ),
-          ),
-          RaisedButton(
-            onPressed: () {
-              context.read<AuthenticationService>().signIn(
-                    email: emailController.text.trim(),
-                    password: passwordController.text.trim(),
-                  );
-            },
-            child: const Text("Sign in"),
-          )
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton(
+                    color: Colors.blue,
+                    onPressed: () {
+                      context.read<AuthenticationService>().signIn(
+                            email: emailController.text.trim(),
+                            password: passwordController.text.trim(),
+                          ).then((value) => _showToast(context, value));
+                    },
+
+                    child: const Text("Sign in"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton(
+                    color: Colors.blue,
+                    onPressed: () {
+                      context.read<AuthenticationService>().signUp(
+                            email: emailController.text.trim(),
+                            password: passwordController.text.trim(),
+                          );
+                    },
+                    child: const Text("Sign up"),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  void _showToast(BuildContext context, String? message) {
+    final scaffold = ScaffoldMessenger.of(context);
+    
+    if(message == "Given String is empty or null"){
+      message = "Please enter email and password";
+    }
+    scaffold.showSnackBar(
+      SnackBar(
+        content: Text(message!),  
       ),
     );
   }
